4b5c72f40d397146cd7c6f0ac718a304791ce3c9
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-01-06 18:28:37 +0000

    email.js sendMail function amended to include subject and reason on email

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,7 +2,8 @@ function sendMail(contactForm) {
     emailjs.send("service_ui3w5sr", "locate-a-loo", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "contact_reason": contactForm.subject.value
+        "subject": contactForm.subject.value,
+        "reason": contactForm.reason.value
     })
     .then(
         function(response) {
