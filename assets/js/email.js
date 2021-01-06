1d80ac2f083e7689b4574b68900e545d36aab632
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-01-06 18:02:06 +0000

    link to email.js added to site

---
+++ assets/js/email.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send('service_ui3w5sr', 'locate-a-loo', {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "contact_reason": contactForm.reason.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
