aa99a897d1d013479dcdc99372bddc33495b4a93
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-01-06 11:42:45 +0000

    Added content and changed text box background color to help it stand out

--- assets/js/home.js
+++ assets/js/home.js
@@ -12,5 +12,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 10000);
+  setTimeout(carousel, 8000);
 }
